bc1b92eced797ae6e7038cbd765ae794e435327f
Rinu-fathima <24mp2291@rit.ac.in>
2024-11-29 13:42:25 +0530

    Create arithmetic.py

---
+++ arithmetic.py
@@ -0,0 +1,16 @@
+num1=int(input("Enter first number:"))
+num2=int(input("Enter second number:"))
+sum=num1+num2
+difference=num1-num2
+product=num1*num2
+division=num1/num2
+modulus=num1%num2
+exponent=num1**num2
+floor_division=num1//num2
+print(f"Sum:{num1}+{num2}={sum}")
+print(f"Difference:{num1}-{num2}={difference}")
+print(f"Product:{num1}*{num2}={product}")
+print(f"Division:{num1}/{num2}={division}")
+print(f"Modulus:{num1}%{num2}={modulus}")
+print(f"Exponent:{num1}**{num2}={exponent}")
+print(f"Floor Division:{num1}//{num2}={floor_division}")
