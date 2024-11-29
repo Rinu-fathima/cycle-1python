6fba57c71b8bfe1a72d3beb7276d873971779a99
Rinu-fathima <24mp2291@rit.ac.in>
2024-11-29 13:49:23 +0530

    Create entryticket.py

---
+++ entryticket.py
@@ -0,0 +1,11 @@
+  GNU nano 4.8                     entryticket.py                               
+age=int(input("Enter your age:"))
+if age<10:
+        print("Your ticket rate for trade is:7");
+elif 10<=age<60:
+        print("your ticket rate for trade fare is:10");
+elif age>=60:
+        print("Your ticket rate for trade fair is:5");
+else:
+        print("Enter a Proper age")
+
