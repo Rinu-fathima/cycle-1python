e2149128b075b7aaefb1b551779c2f749a0d6fa9
Rinu-fathima <24mp2291@rit.ac.in>
2024-11-29 13:48:31 +0530

    Create leapyear.py

---
+++ leapyear.py
@@ -0,0 +1,11 @@
+  GNU nano 4.8                      leapyear.py                                 
+year=int(input("Enter a year:"))
+if year%4!=0:
+        print(f"{year} is not a leap year")
+elif year%100!=0:
+        print(f"{year} is a leap year")
+elif year%400==0:
+        print(f"{year} is a leap year")
+else:
+        print(f"{year} is not a leap year")
+
